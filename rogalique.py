85c6714d0bd4251ee881bf517c69c7fa87b753b1
Ashatanka <sarzhanovan@yandex.ru>
2024-02-21 00:11:44 +0300

    lines generates

--- rogalique.py
+++ rogalique.py
@@ -17,7 +17,7 @@ print("rooms_number = ", rooms_number)
 for room in range(1, rooms_number+1):
     room_x = random.randint(1, map_width)
     room_y = random.randint(1, map_height)
-    room_width = random.randint(room_minsize, room_maxsize) #4
+    room_width = random.randint(room_minsize, room_maxsize)
     if ((map_width-room_x) < room_width):
         room_width = map_width-room_x
     room_height = random.randint(room_minsize, room_maxsize)
@@ -27,6 +27,24 @@ for room in range(1, rooms_number+1):
     for height in range(room_height):
         for width in range(room_width):
             map_grid[room_y+height-1][room_x+width-1] = ' '
+
+# генерация линий
+# to do:
+# обеспечить доступность каждой комнаты
+# линии не должны оказаться друг в друге - в функцию подавать уже занятые места
+# линии не должны быть рядом (ширина прохода 1 блок)
+horizontal_lines_number = random.randint(3, 5)
+print("horiz = ", horizontal_lines_number)
+vertical_lines_number = random.randint(3, 5)
+print("vert = ", vertical_lines_number)
+
+for _ in range(horizontal_lines_number):
+    map_grid[random.randint(1, map_height)-1] = [' ']*map_width
+
+for _ in range(vertical_lines_number):
+    col = random.randint(1, map_width)
+    for row in map_grid:
+        row[col-1] = ' '
 
 # Вывод карты для проверки
 for row in map_grid:
